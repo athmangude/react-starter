b6f6ae948da3785fb359c9be9d2ca1d2da777723
Athman Gude <athmangude@gmail.com>
2016-04-20 20:14:49 +0300

    active links

--- modules/App.js
+++ modules/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+import NavLink from './NavLink';
+
 export default React.createClass({
     render() {
         return (
             <div>
                 <h1>React Router Tutorial</h1>
                 <ul role="nav">
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/repos">Repos</Link></li>
+                    <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
+                    <li><NavLink to="/repos" activeClassName="active">Repos</NavLink></li>
                 </ul>
                 {this.props.children}
             </div>

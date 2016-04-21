5e9e05532eb24be2bd670c1495b55ef89883fbeb
Athman Gude <athmangude@gmail.com>
2016-04-21 21:03:29 +0300

    more nesting

--- modules/Repos.js
+++ modules/Repos.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Link } from 'react-router';
+import NavLink from './NavLink';
 
 export default React.createClass({
     render() {
@@ -8,9 +9,11 @@ export default React.createClass({
             <div>
                 <h2>Repos</h2>
                 <ul>
-                    <li><Link to="/repos/reactjs/react-router">React Router</Link></li>
-                    <li><Link to="/repos/facebook/react">React</Link></li>
+                    <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
+                    <li><NavLink to="/repos/facebook/react">React</NavLink></li>
                 </ul>
+                {/* will render Repos.js when at /repos/:userName/:repoName */}
+                {this.props.children}
             </div>
         );
     }

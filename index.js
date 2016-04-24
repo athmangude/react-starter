5df3ef55f677a5791c49949ab974534e3ef99cdc
Athman Gude <athmangude@gmail.com>
2016-04-25 02:04:18 +0300

    creating the store, linking up the store to the app via the router

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, browserHistory } from 'react-router';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import rootReducer from './reducers';
 
 // import routes and pass them into <Router />
 import routes from './components/routes';
 
+const store = createStore(rootReducer,  window.devToolsExtension ? window.devToolsExtension() : f => f);
+
 render(
-    <Router routes={routes} history={browserHistory} />, document.getElementById('app')
+    <Provider store={store}>
+        <Router routes={routes} history={browserHistory} />
+    </Provider>,
+    document.getElementById('app')
 );

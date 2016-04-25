99acb437ee936ff11cb2c7ea7eaf72d844792546
Athman Gude <athmangude@gmail.com>
2016-04-26 00:37:03 +0300

    creating socketConnection factory

--- components/socket-connection/index.js
+++ components/socket-connection/index.js
@@ -1,6 +1,44 @@
 import * as authenticationActions from '../../actions/authentication-actions';
 
-class SocketConnection {
+// let socketConnection = null;
+//
+// let getSocketUrl = () => {
+//     return window.location.host.includes('app.bambapos.com') ? 'ws://178.238.233.77:8090/websocket' : 'ws://91.194.91.50:8090/websocket';
+// }
+//
+// let send = (payload) => {
+//     socketConnection.send(JSON.stringify(payload));
+// }
+//
+// export default (store) => {
+//     socketConnection = new WebSocket(getSocketUrl());
+//
+//     socketConnection.onopen = function () {
+//         console.log('socket opened');
+//     }
+//
+//     socketConnection.onmessage = function (message) {
+//         console.log('message received', message);
+//
+//         switch (message.channel) {
+//             case 'SIGNING_IN':
+//                 store.dispatch(authenticationActions.signIn(message));
+//                 break;
+//             default:
+//
+//         }
+//     }
+//
+//     socketConnection.onclose = function () {
+//         console.log('socket closed');
+//     }
+//
+//     socketConnection.onerror = function () {
+//         console.log('socket erred');
+//     }
+// }
+
+export default (store) => class SocketConnection {
     constructor(store) {
         this.socketConnection = new WebSocket(this.getSocketUrl());
 
@@ -37,5 +75,3 @@ class SocketConnection {
         this.socketConnection.send(JSON.stringify(payload));
     }
 }
-
-export default new SocketConnection();

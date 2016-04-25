f025634eb35e160caa762733ca380f4fb96ccec5
Athman Gude <athmangude@gmail.com>
2016-04-25 11:04:42 +0300

    modularizing reducers

---
+++ reducers/authentication/index.js
@@ -0,0 +1,15 @@
+const initialState = {
+    isLoggedIn: false,
+    isSigningIn: false
+}
+
+const authentication = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SIGNING_IN':
+            return Object.assign({}, state, { isSigningIn: true });
+        default:
+            return state;
+    }
+}
+
+export default authentication;

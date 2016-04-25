0bdd4de9ddc2d4a294ceabbc82f37ea94617ed4b
Athman Gude <athmangude@gmail.com>
2016-04-25 11:01:07 +0300

    combining reducers to rootReducer

--- reducers/index.js
+++ reducers/index.js
@@ -1,10 +1,9 @@
-const rootReducer = (state, action = 'SIGNING_IN') => {
-    switch (action.type) {
-        case 'SIGNING_IN':
-            return Object.assign({}, state, { isSigningIn: true });
-        default:
-            return state;
-    }
-}
+import { combineReducers } from 'redux';
+
+import authentication from './authentication/index.js';
+
+const rootReducer = combineReducers({
+    authentication
+});
 
 export default rootReducer;

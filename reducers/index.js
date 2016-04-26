a11325e2db789968ef190f78c7aef5155f497c61
Athman Gude <athmangude@gmail.com>
2016-04-26 14:09:28 +0300

    combining app reducer

--- reducers/index.js
+++ reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import authentication from './authentication/index.js';
+import app from './app/index.js';
 
 const rootReducer = combineReducers({
-    authentication
+    authentication,
+    app
 });
 
 export default rootReducer;

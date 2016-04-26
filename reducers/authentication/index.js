94140caffc8b2f727c73685fcccf9e374bdceffe
Athman Gude <athmangude@gmail.com>
2016-04-26 09:37:01 +0300

    implemented complete sign in action

--- reducers/authentication/index.js
+++ reducers/authentication/index.js
@@ -7,6 +7,11 @@ const authentication = (state = initialState, action) => {
     switch (action.type) {
         case 'SIGN_IN':
             return Object.assign({}, state, { isSigningIn: true });
+        case 'COMPLETE_SIGN_IN':
+            if (action.data.status) {
+                return Object.assign({}, state, { isSigningIn: false, isLoggedIn: true });
+            }
+            return Object.assign({}, state, { isSigningIn: false });
         default:
             return state;
     }

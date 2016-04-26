f5987048eb0d4352912e3ab9149facb7cbbb05e9
Athman Gude <athmangude@gmail.com>
2016-04-27 01:52:38 +0300

    toggling the left nav

--- components/side-bar/index.js
+++ components/side-bar/index.js
@@ -2,22 +2,37 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import { LeftNav, AppBar, RaisedButton } from 'material-ui';
+import { LeftNav, MenuItem } from 'material-ui';
+
+import * as appActions from '../../actions/app-actions';
 
 @connect(state => ({app: state.app}))
 class SideBar extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            open: true
+        }
+    }
+
+    handleToggle = () => {
+        this.props.dispatch(appActions.toggleSideBar());
     }
 
     render() {
-        console.log(this.props.app.isSideBarOpen);
-
         const { app } = this.props;
         return (
             <div>
-                <LeftNav width={200} open={app.isSideBarOpen} >
-                    <AppBar title="AppBar"/>
+                <LeftNav
+                    onBlur={this.handleToggle}
+                    width={200}
+                    open={app.isSideBarOpen}
+                    docked={false}
+                    onRequestChange={open => {this.handleToggle()}}
+                >
+                    <MenuItem onTouchTap={this.handleToggle}>Menu Item</MenuItem>
+                    <MenuItem onTouchTap={this.handleToggle}>Menu Item 2</MenuItem>
                 </LeftNav>
             </div>
         );

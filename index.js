15082524f3580feecd7d342f363962f005eafc39
Athman Gude <athmangude@gmail.com>
2016-04-26 09:36:11 +0300

    imported socket connection, socket connection middle and applyMiddleware

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, browserHistory } from 'react-router';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 import rootReducer from './reducers';
 
-import SocketConnectionFactory from './components/socket-connection';
+import openSocket, { socketConnectionMiddleWare } from './components/socket-connection';
 
 // import routes and pass them into <Router />
 import routes from './components/routes';
-
-const store = createStore(rootReducer,  window.devToolsExtension ? window.devToolsExtension() : f => f);
-let SocketConnectionClass = SocketConnectionFactory();
-let SocketConnection = new SocketConnectionClass(store);
+const createStoreWithMiddleware = applyMiddleware(socketConnectionMiddleWare)(createStore);
+const store = createStoreWithMiddleware(rootReducer,  window.devToolsExtension ? window.devToolsExtension() : f => f);
+openSocket(store);
 
 render(
     <Provider store={store}>

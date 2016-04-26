3aecae113d865ff7f0b9121542cb1bd2a08b1c34
Athman Gude <athmangude@gmail.com>
2016-04-26 14:20:51 +0300

    wrapping loadApp functionality in a timeout to show progress

--- actions/app-actions/index.js
+++ actions/app-actions/index.js
@@ -9,13 +9,14 @@ export const finishLoading = (status) => {
 
 export const loadApp = () => {
     return (dispatch) => {
+        setTimeout(function () {
+            if (localStorage.getItem('isSignedIn') && localStorage.getItem('user') ) {
+                dispatch(finishLoading(localStorage.getItem('isSignedIn')));
+                dispatch(authenticationActions.completeSignIn(JSON.parse(localStorage.getItem('user'))));
+                return;
+            }
 
-        if (localStorage.getItem('isSignedIn') && localStorage.getItem('user') ) {
-            dispatch(finishLoading(localStorage.getItem('isSignedIn')));
-            dispatch(authenticationActions.completeSignIn(JSON.parse(localStorage.getItem('user'))));
-            return;
-        }
-
-        return dispatch(finishLoading(false));
+            return dispatch(finishLoading(false))
+        }, 1000);
     }
 }

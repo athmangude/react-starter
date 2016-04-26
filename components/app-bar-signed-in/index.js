43bae0d751ca00111a1d55ee2c7329825bed85c4
Athman Gude <athmangude@gmail.com>
2016-04-26 23:26:46 +0300

    connecting side bar to the store, showing the sidebar

--- components/app-bar-signed-in/index.js
+++ components/app-bar-signed-in/index.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'react';
 
+import { AppBar, FlatButton, IconButton, IconMenu, MenuItem, CircularProgress } from 'material-ui';
 import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
-import { AppBar, FlatButton, NavigationClose, IconButton, IconMenu, MenuItem, CircularProgress } from 'material-ui';
+import NavigationClose from 'material-ui/lib/svg-icons/navigation/close';
 
+import * as appActions from '../../actions/app-actions';
+
+@connect(state => ({app: state.app}))
 class AppBarSignedIn extends Component {
+    handleToggle = () => {
+        this.props.dispatch(appActions.toggleSideBar());
+    }
+
     render () {
+
         return (
             <AppBar
             title="Bambapos"
+            iconElementLeft={
+                <IconButton
+                    onFocus={this.handleToggle} >
+                    <NavigationClose />
+                </IconButton>
+            }
             iconElementRight={
                 <IconMenu
                 iconButtonElement={

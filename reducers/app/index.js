21cbe6e1e01a1c151f4cbaf68af5af580fdaf2d8
Athman Gude <athmangude@gmail.com>
2016-04-26 23:27:52 +0300

    implemnted reducer for toggling sidebar

--- reducers/app/index.js
+++ reducers/app/index.js
@@ -1,11 +1,14 @@
 const initialState = {
     isLoading: true,
+    isSideBarOpen: false
 }
 
 const app = (state = initialState, action) => {
     switch (action.type) {
         case 'FINISH_LOADING':
             return Object.assign({}, state, { isLoading: false});
+        case 'TOGGLE_SIDE_BAR':
+            return Object.assign({}, state, { isSideBarOpen: !state.isSideBarOpen })
         default:
             return state;
     }

f3253eb2807a2d439e7ef735e8f22004e420fd60
Athman Gude <athmangude@gmail.com>
2016-04-27 23:09:47 +0300

    wrapping it in a theme provider

--- components/loaders/app-loading/index.js
+++ components/loaders/app-loading/index.js
@@ -1,29 +1,33 @@
 import React, { Component } from 'react';
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import { CircularProgress, Paper } from 'material-ui';
 
 class AppLoading extends Component {
     render () {
         return (
-            <div style={{
-                height: '100vh',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                flexDirection: 'column'
-            }}>
-                <CircularProgress />
-                <Paper
-                    style={{
-                        textAlign: 'center',
-                        marginTop: 10,
-                        backgroundColor: 'transparent'
-                    }}
-                    zDepth={0}
-                >
-                    <span>Loading Bambapos</span>
-                </Paper>
-            </div>
+            <MuiThemeProvider muiTheme={getMuiTheme()}>
+                <div style={{
+                    height: '100vh',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    flexDirection: 'column'
+                }}>
+                    <CircularProgress />
+                    <Paper
+                        style={{
+                            textAlign: 'center',
+                            marginTop: 10,
+                            backgroundColor: 'transparent'
+                        }}
+                        zDepth={0}
+                    >
+                        <span>Loading Bambapos</span>
+                    </Paper>
+                </div>
+            </MuiThemeProvider>
         );
     }
 }

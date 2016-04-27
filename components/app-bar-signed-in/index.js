e165a564d26eb7a94ab71d36780ee2050e789764
Athman Gude <athmangude@gmail.com>
2016-04-27 23:08:18 +0300

    wrapping it in a theme provider

--- components/app-bar-signed-in/index.js
+++ components/app-bar-signed-in/index.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'react';
 
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { AppBar, FlatButton, IconButton, IconMenu, MenuItem, CircularProgress } from 'material-ui';
-import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
-import NavigationClose from 'material-ui/lib/svg-icons/navigation/close';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
 
 import * as appActions from '../../actions/app-actions';
 
@@ -12,23 +14,25 @@ import * as appActions from '../../actions/app-actions';
 class AppBarSignedIn extends Component {
     render () {
         return (
-            <AppBar
-                title="Bambapos"
-                onLeftIconButtonTouchTap={this.props.handleToggle}
-                iconElementRight={
-                    <IconMenu
-                    iconButtonElement={
-                        <IconButton><MoreVertIcon /></IconButton>
+            <MuiThemeProvider muiTheme={getMuiTheme()}>
+                <AppBar
+                    title="Bambapos"
+                    onLeftIconButtonTouchTap={this.props.handleToggle}
+                    iconElementRight={
+                        <IconMenu
+                        iconButtonElement={
+                            <IconButton><MoreVertIcon /></IconButton>
+                        }
+                        targetOrigin={{horizontal: 'right', vertical: 'top'}}
+                        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+                        >
+                        <MenuItem primaryText="Refresh" />
+                        <MenuItem primaryText="Help" />
+                        <MenuItem primaryText="Sign out" />
+                        </IconMenu>
                     }
-                    targetOrigin={{horizontal: 'right', vertical: 'top'}}
-                    anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-                    >
-                    <MenuItem primaryText="Refresh" />
-                    <MenuItem primaryText="Help" />
-                    <MenuItem primaryText="Sign out" />
-                    </IconMenu>
-                }
-            />
+                />
+            </MuiThemeProvider>
         );
     }
 }

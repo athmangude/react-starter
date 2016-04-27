8c5c67f7693a379286114891c5d8c37cf8890fe1
Athman Gude <athmangude@gmail.com>
2016-04-27 19:10:06 +0300

    added icons to side bar menu items

--- components/side-bar/index.js
+++ components/side-bar/index.js
@@ -3,9 +3,15 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import NavLink from '../nav-link';
 
-import { LeftNav, MenuItem } from 'material-ui';
+import { LeftNav, MenuItem, FontIcon, Colors } from 'material-ui';
 
 import * as appActions from '../../actions/app-actions';
+
+const iconStyles = {
+    marginRight: 12,
+    position: 'relative',
+    top: 6
+};
 
 @connect(state => ({app: state.app}))
 class SideBar extends React.Component {
@@ -34,34 +40,64 @@ class SideBar extends React.Component {
                 >
                     <NavLink
                         to="/dashboard">
-                        <MenuItem onTouchTap={this.handleToggle}>Dashboard</MenuItem>
+                        <MenuItem onTouchTap={this.handleToggle}>
+                            <FontIcon className="material-icons" style={iconStyles}>dashboard</FontIcon>
+                            Dashboard
+                        </MenuItem>
                     </NavLink>
                     <NavLink to="/inventory">
-                        <MenuItem onTouchTap={this.handleToggle}>Inventory</MenuItem>
+                        <MenuItem onTouchTap={this.handleToggle}>
+                            <FontIcon className="material-icons" style={iconStyles}>book</FontIcon>
+                            Inventory
+                        </MenuItem>
                     </NavLink>
                     <NavLink to="/products">
-                        <MenuItem onTouchTap={this.handleToggle}>Products</MenuItem>
+                        <MenuItem onTouchTap={this.handleToggle}>
+                            <FontIcon className="material-icons" style={iconStyles}>local_mall</FontIcon>
+                            Products
+                        </MenuItem>
                     </NavLink>
                     <NavLink to="/categories">
-                        <MenuItem onTouchTap={this.handleToggle}>Categories</MenuItem>
+                        <MenuItem onTouchTap={this.handleToggle}>
+                            <FontIcon className="material-icons" style={iconStyles}>local_offer</FontIcon>
+                            Categories
+                        </MenuItem>
                     </NavLink>
                     <NavLink to="/suppliers">
-                        <MenuItem onTouchTap={this.handleToggle}>Suppliers</MenuItem>
+                        <MenuItem onTouchTap={this.handleToggle}>
+                            <FontIcon className="material-icons" style={iconStyles}>person</FontIcon>
+                            Suppliers
+                        </MenuItem>
                     </NavLink>
                     <NavLink to="/outlets">
-                        <MenuItem onTouchTap={this.handleToggle}>Outlets</MenuItem>
+                        <MenuItem onTouchTap={this.handleToggle}>
+                            <FontIcon className="material-icons" style={iconStyles}>store</FontIcon>
+                            Outlets
+                        </MenuItem>
                     </NavLink>
                     <NavLink to="/customers">
-                        <MenuItem onTouchTap={this.handleToggle}>Customers</MenuItem>
+                        <MenuItem onTouchTap={this.handleToggle}>
+                            <FontIcon className="material-icons" style={iconStyles}>people</FontIcon>
+                            Customers
+                        </MenuItem>
                     </NavLink>
                     <NavLink to="/reports">
-                        <MenuItem onTouchTap={this.handleToggle}>Reports</MenuItem>
+                        <MenuItem onTouchTap={this.handleToggle}>
+                            <FontIcon className="material-icons" style={iconStyles}>equalizer</FontIcon>
+                            Reports
+                        </MenuItem>
                     </NavLink>
                     <NavLink to="/staff">
-                        <MenuItem onTouchTap={this.handleToggle}>Staff</MenuItem>
+                        <MenuItem onTouchTap={this.handleToggle}>
+                            <FontIcon className="material-icons" style={iconStyles}>people_outline</FontIcon>
+                            Staff
+                        </MenuItem>
                     </NavLink>
                     <NavLink to="/settings">
-                        <MenuItem onTouchTap={this.handleToggle}>Settings</MenuItem>
+                        <MenuItem onTouchTap={this.handleToggle}>
+                            <FontIcon className="material-icons" style={iconStyles}>settings</FontIcon>
+                            Settings
+                        </MenuItem>
                     </NavLink>
                 </LeftNav>
             </div>

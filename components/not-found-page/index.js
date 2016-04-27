52b022bbb141275f520c502ffbce4cc5a64ff2af
Athman Gude <athmangude@gmail.com>
2016-04-28 01:18:59 +0300

    wrapped it around mui theme provider

--- components/not-found-page/index.js
+++ components/not-found-page/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Paper } from 'material-ui';
 
 const styles = {
@@ -17,10 +19,12 @@ class NotFoundPage extends Component {
 
     render() {
         return (
-            <Paper style={styles} zDepth={0}>
-                <h4>404</h4>
-                <span>Page Not Found</span>
-            </Paper>
+            <MuiThemeProvider muiTheme={getMuiTheme()}>
+                <Paper style={styles} zDepth={0}>
+                    <h4>404</h4>
+                    <span>Page Not Found</span>
+                </Paper>
+            </MuiThemeProvider>
         );
     }
 }

954c310997689955393b384dfdf2a4ff96bb3f6a
Athman Gude <athmangude@gmail.com>
2016-04-28 16:07:11 +0300

    removed app theme

--- index.js
+++ index.js
@@ -8,20 +8,19 @@ import { routerMiddleware } from 'react-router-redux'
 
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import appTheme from './appTheme';
 
 import rootReducer from './reducers';
 
 import openSocket, { socketConnectionMiddleWare } from './components/socket-connection';
 
-// import routes and pass them into <Router />
 import routes from './components/routes';
+
 const createStoreWithMiddleware = applyMiddleware(socketConnectionMiddleWare, thunk, routerMiddleware(browserHistory))(createStore);
 const store = createStoreWithMiddleware(rootReducer,  window.devToolsExtension ? window.devToolsExtension() : f => f);
 openSocket(store);
 
 const Bambapos = () => (
-    <MuiThemeProvider muiTheme={getMuiTheme(appTheme)}>
+    <MuiThemeProvider muiTheme={getMuiTheme()}>
         <Provider store={store}>
             <Router routes={routes} history={browserHistory} />
         </Provider>

d4c6ac83b7ff25d42c70bc03874b128a133c63d1
Athman Gude <athmangude@gmail.com>
2016-04-28 14:22:33 +0300

    imported app theme

--- components/app-container/index.js
+++ components/app-container/index.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import appTheme from '../../appTheme'
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 

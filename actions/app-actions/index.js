38e733d2f6d00a7d020d0be5fa414db0c232cc88
Athman Gude <athmangude@gmail.com>
2016-04-28 09:21:38 +0300

    added routes to routeToIndex action

--- actions/app-actions/index.js
+++ actions/app-actions/index.js
@@ -1,3 +1,5 @@
+import { push } from 'react-router-redux'
+
 import * as authenticationActions from '../authentication-actions/index.js';
 
 export const finishLoading = (status) => {
@@ -36,7 +38,66 @@ export const routeToIndex = (index) => {
     return (dispatch) => {
         setTimeout(function () {
             console.log('routing to id');
-            return dispatch(changeSelectedDrawerMenuItem(index));
+            dispatch(changeSelectedDrawerMenuItem(index));
+
+            let path = null;
+            switch (index) {
+                case 1:
+                    path = '/dashboard';
+                    break;
+                case 2:
+                    path = '/transactions';
+                    break;
+                case 3:
+                    path = '/reports';
+                    break;
+                case 4:
+                    path = '/reports/general-sales';
+                    break;
+                case 5:
+                    path = '/reports/product-sales';
+                    break;
+                case 6:
+                    path = '/reports/branch-sales';
+                    break;
+                case 7:
+                    path = '/reports/staff';
+                    break;
+                case 8:
+                    path = '/reports/inventory';
+                    break;
+                case 9:
+                    path = '/reports/reorder-levels';
+                    break;
+                case 10:
+                    path = '/inventory';
+                    break;
+                case 11:
+                    path = '/inventory/products';
+                    break;
+                case 12:
+                    path = '/inventory/categories';
+                    break;
+                case 13:
+                    path = '/inventory/suppliers';
+                    break;
+                case 14:
+                    path = '/inventory/outlets';
+                    break;
+                case 15:
+                    path = '/customers';
+                    break;
+                case 16:
+                    path = '/staff';
+                    break;
+                case 17:
+                    path = '/settings';
+                    break;
+                default:
+                    path = '/404';
+            }
+
+            dispatch(push(path));
         }, 400);
     }
 }

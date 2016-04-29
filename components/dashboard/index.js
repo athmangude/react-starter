fc2f23b660d0a26461c78d4d8baa10ef1c106ced
Athman Gude <athmangude@gmail.com>
2016-04-29 09:06:20 +0300

    testing out react-bootstrap grid system

--- components/dashboard/index.js
+++ components/dashboard/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 class Dashboard extends Component {
     constructor(props) {
@@ -7,7 +8,25 @@ class Dashboard extends Component {
 
     render () {
         return (
-            <div>Dashboard</div>
+            <Grid>
+                <Row>
+                    <Col xs={12} md={8}><code>&lt;{'Col xs={12} md={8}'} /&gt;</code></Col>
+                    <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+                </Row>
+                <Row>
+                  <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+                  <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+                  <Col xsHidden md={4}><code>&lt;{'Col xsHidden md={4}'} /&gt;</code></Col>
+                </Row>
+                <Row>
+                  <Col xs={6} xsOffset={6}><code>&lt;{'Col xs={6} xsOffset={6}'} /&gt;</code></Col>
+                </Row>
+
+                <Row>
+                  <Col md={6} mdPush={6}><code>&lt;{'Col md={6} mdPush={6}'} /&gt;</code></Col>
+                  <Col md={6} mdPull={6}><code>&lt;{'Col md={6} mdPull={6}'} /&gt;</code></Col>
+                </Row>
+            </Grid>
         )
     }
 }

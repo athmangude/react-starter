86745b10cbacd41dfb0c99533ed007eb05d424bd
Athman Gude <athmangude@gmail.com>
2016-04-29 11:33:44 +0300

    testing sass styles

--- components/dashboard/index.js
+++ components/dashboard/index.js
@@ -5,6 +5,8 @@ import { Paper } from 'material-ui';
 import CardsWrapper from './cards-wrapper';
 import DashboardCard from './dashboard-card';
 
+import {} from './style.scss';
+
 class Dashboard extends Component {
     constructor(props) {
         super(props);
@@ -12,7 +14,7 @@ class Dashboard extends Component {
 
     render () {
         return (
-            <Grid fluid={true}>
+            <Grid className="custom-color" fluid={true}>
                 <Row>
                     <Col lg={3} md={4} sm={6} xs={12}>
                         <DashboardCard title={"Total Sales"} value={"8,904,940.00"} className="col-lg-3 col-md-4 col-sm-6 col-xs-12" />

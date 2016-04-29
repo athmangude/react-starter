aa4ac42a5cf0880f8ee71bb983d3c47f1012e460
Athman Gude <athmangude@gmail.com>
2016-04-29 14:08:29 +0300

    centering col

--- components/dashboard/index.jsx
+++ components/dashboard/index.jsx
@@ -4,6 +4,7 @@ import { Paper } from 'material-ui';
 
 import CardsWrapper from './cards-wrapper';
 import DashboardCard from './dashboard-card';
+import LessPaddingCol from '../utils/less-padding-col';
 
 import {} from './style.scss';
 
@@ -14,17 +15,17 @@ class Dashboard extends Component {
 
     render () {
         return (
-            <Grid className="custom-color" fluid={true}>
-                <Row>
-                    <Col lg={3} md={4} sm={6} xs={12}>
+            <Grid fluid={true}>
+                <Row className="row-centered">
+                    <LessPaddingCol lg={3} md={4} sm={6} xs={12} className="col-centered">
                         <DashboardCard title={"Total Sales"} value={"8,904,940.00"} className="col-lg-3 col-md-4 col-sm-6 col-xs-12" />
-                    </Col>
-                    <Col lg={3} md={4} sm={6} xs={12}>
+                    </LessPaddingCol>
+                    <LessPaddingCol lg={3} md={4} sm={6} xs={12} className="col-centered">
                         <DashboardCard title={"Total Sales"} value={"8,904,940.00"} className="col-lg-3 col-md-4 col-sm-6 col-xs-12" />
-                    </Col>
-                    <Col lg={3} md={4} sm={6} xs={12}>
+                    </LessPaddingCol>
+                    <LessPaddingCol lg={3} md={4} sm={6} xs={12} className="col-centered">
                         <DashboardCard title={"Total Sales"} value={"8,904,940.00"} className="col-lg-3 col-md-4 col-sm-6 col-xs-12" />
-                    </Col>
+                    </LessPaddingCol>
                 </Row>
             </Grid>
         )
